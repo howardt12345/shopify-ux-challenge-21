cc2057faa5a8408fe4928bc91db4dcd6724f4359
Howard Tseng <howardt12345@users.noreply.github.com>
2021-01-08 22:28:57 -0500

    Info dialog

---
+++ src/components/info_dialog.js
@@ -0,0 +1,67 @@
+import React, { useContext } from 'react';
+import { Dialog } from '@reach/dialog';
+import "@reach/dialog/styles.css"
+import styled, { ThemeContext } from 'styled-components';
+import { Github } from 'styled-icons/boxicons-logos';
+
+import { media } from '../styles';
+import { Button } from '../components';
+
+const StyledDialog = styled(Dialog)`
+  width: 600px;
+  ${media.tablet`width: 80vw;`};
+  background-color: ${props => props.theme.bg}
+`;
+const StyledContainer = styled.div`
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+  margin-bottom: 0.5rem;
+`;
+const StyledTitle = styled.h1`
+  font-size: 2rem;
+  text-align: left;
+  line-height: 1;
+`;
+const StyledInfoContent = styled.p`
+  text-align: left;
+  line-height: 1.5;
+`;
+const StyledFooterLink = styled.a`
+  display:block;
+  text-align: center;
+  font-size: 0.8em;
+  flex-direction: column;
+`;
+export const InfoDialog = ({ isOpen, onClose }) => {
+
+  const themeContext = useContext(ThemeContext);
+
+  return (
+    <StyledDialog
+      isOpen={isOpen}
+      onDismiss={onClose}
+      theme={themeContext}
+    >
+      <StyledContainer>
+        <StyledTitle>About</StyledTitle>
+        <StyledInfoContent>Created by Howard Tseng for the Shopify UX Developer Intern & Web Developer Intern (Summer 2021) Challenge. This project uses React and Styled Components, and is designed with both desktop and mobile layouts in mind.</StyledInfoContent>
+        <StyledInfoContent>Hoping whoever sees this likes the application!</StyledInfoContent>
+        <Button 
+          href="https://github.com/howardt12345/shopify-ux-challenge-21"
+          target="_blank"
+          rel="nofollow noopener noreferrer">
+          <Github />
+        </Button>
+        <StyledFooterLink
+          href="https://howardt12345.com"
+          target="_blank"
+          rel="nofollow noopener noreferrer"
+        >
+          <div>{`© ${new Date().getFullYear()} Howard Tseng`}</div>
+        </StyledFooterLink>
+      </StyledContainer>
+      <button onClick={onClose}>Close</button>
+    </StyledDialog>
+  );
+}
